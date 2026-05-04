721282b38d641ad01ea2076a8473df554e28eeda
irvy32 <irvy32@gmail.com>
2026-05-04 11:27:26 +0800

    fix: 修正為 React 版設定並加入 base 路徑

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vue/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/travel-english-app/', // 加上這一行，必須與您的儲存庫名稱一致
+  plugins: [react()],
+  base: '/travel-english-app/', 
 })
